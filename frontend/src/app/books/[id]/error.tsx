a165ded74ea9c641573e759ec8045739910fc307
Alex Huang <zthh626@gmail.com>
2024-06-24 21:21:39 -0700

    feat: error page on 404 for book

---
+++ frontend/src/app/books/[id]/error.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function BookError() {
+  return (
+    <div className="flex flex-col justify-center items-center space-y-2 w-full h-full">
+      <h2 className="text-2xl font-bold">404 Error</h2>
+      <p>Book not found.</p>
+    </div>
+  );
+}
